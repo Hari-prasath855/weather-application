86cfd91c604a0e44db5d668153688fbb99c203c3
Hari Prasath <68268300+Hari-prasath855@users.noreply.github.com>
2023-07-28 01:27:08 +0530

    weather application

--- weather-app(reactapp)/src/App.js
+++ weather-app(reactapp)/src/App.js
@@ -1,1 +1,63 @@
+import React, { useEffect, useState } from "react";
+import Card from "./components/Card";
+import{get} from "./axios"
+import './App.css'
 
+function App() {
+  const [cityData, setCityData] = useState({});
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const getData = async (city) => {
+    await get(`${'/weather'}/${city}`).then((response) => {
+      setCityData(response);
+    }).catch(error => {
+      console.log(error);
+    });
+  }
+
+  useEffect(() => {
+    getData('Coimbatore');
+  }, []);
+  
+
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const handleSearchClick = () => {
+    getData(searchQuery);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handleSearchClick();
+    }
+  };
+
+  return (
+    <div className="box-container">
+      <div className="box-header">
+        <h1 style={{ textAlign: 'center' }}>
+          Weather Application
+        </h1>
+        <div className="topright">
+          <input
+            className="box-input"
+            type="search"
+            placeholder="Enter City Name..."
+            value={searchQuery}
+            onChange={handleSearchChange}
+            onKeyPress={handleKeyPress}
+          />
+          <button className="box-searchButton" onClick={handleSearchClick}>🔍</button>
+        </div>
+      </div>
+
+      <div className="box-cardContainer">
+        <Card {...cityData} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
